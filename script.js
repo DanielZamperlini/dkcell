1a88e9312e3e79bec2cae7256cad85bb9f017e8c
Daniel Santos <monkeydluffysan2021@gmail.com>
2024-02-23 17:18:18 -0300

    modo escuro/claro

--- script.js
+++ script.js
@@ -141,3 +141,18 @@ function gerarPDF() {
   // Salvar o PDF ou exibir no navegador
   pdf.save('OrdemDeServico.pdf');
 }
+
+//mudar cor do css
+const button = document.getElementById('changeColorButton');
+let number = 0;
+
+button.addEventListener('click', function () {
+  const ativar = document.querySelector('body');
+  if (number === 0) {
+    number = 1;
+  } else {
+    ativar.classList.toggle('ativo');
+    number = 0;
+    console.log('certo');
+  }
+});
